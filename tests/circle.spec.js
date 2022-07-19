02bbeb46388eea82442a88a91954c95404850c41
Thierry <varelathierry@gmail.com>
2022-07-19 17:56:32 -0300

    exercicio 4 testes circle

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -3,6 +3,9 @@
 
 const circle = require('../src/circle');
 
+expect(circle(1)).toEqual({radius: 1, area: 3.14, circumference: 6.28})
+expect(circle(7)).toEqual({radius: 7, area: 153.86, circumference: 43.96})
+
 /*
   A função `circle` recebe o raio de um círculo e retorna um objeto contendo suas informações: Raio, Área e Circunferência.
   Se não for especificado um raio, a função retorna `undefined`.
@@ -23,14 +26,28 @@ const circle = require('../src/circle');
 
 describe('4 - Implemente os casos de teste para a função `circle`', () => {
   it('Verifica se ao receber um raio, a função `circle` retorna um objeto contendo os valores esperados', () => {
-    fail('Teste vazio!');
+    // expect(circle(1)).toMatch(/{radius: 1, area: 3.14, circumference: 6.28}/);
+
+    
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna undefined, caso o parâmetro passado não seja um número.
+
+    expect(circle('l')).toBeUndefined();
+
     // Teste se circle retorna um objeto.
+    expect(typeof circle(1)).toBe('object');
+
     // Teste se o objeto retornado possui 3 propriedades.
+    expect(Object.keys(circle(1)).length).toBe(3);
+
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    expect(circle()).toBe(undefined);
     // Teste se dentro do objeto retornado, a função retorna uma `key` com `value` igual à circunferência correta para um círculo de raio 2.
+    expect((Object.values(circle(2))[2])).toBe(12.56);
     // Teste se dentro do objeto retornado, a função retorna uma `key` com `value` igual à área correta para um círculo de raio 3.
+    expect((Object.values(circle(3))[1])).toBe(28.259999999999998);
     // Teste se a função retorna, em um objeto, os dados corretos de um círculo de raio 3.
+    expect((Object.values(circle(3))[0])).toBe(3)
+    expect((Object.values(circle(3))[2])).toBe(18.84)
   });
 });
