15a990edaa7f15e71ba0d88e4ed2b5537a62e561
Thierry <varelathierry@gmail.com>
2022-07-19 15:45:44 -0300

    implementando funcao que retorna pagrafo

--- src/average.js
+++ src/average.js
@@ -1,14 +1,14 @@
 const average = (myArray) => {
   let valor = 0;
-  if (myArray.length === 0) { return undefined}
+  if (myArray.length === 0) { return undefined; }
   for (let i = 0; i < myArray.length; i += 1) {
-    valor +=myArray[i];
+    valor += myArray[i];
     if (typeof myArray[i] !== 'number') {
       return undefined;
     }
   }
-  let resultado = Math.round(valor/myArray.length)
-  return resultado
+  let resultado = Math.round(valor / myArray.length);
+  return resultado;
 };
 
 module.exports = average;
